0323d5a25c85744115fafac5407778fdeb61a4c4
magnus <magnus@udel.edu>
2025-03-03 22:53:16 -0500

    added sumPublishedPoints

--- src/nested.ts
+++ src/nested.ts
@@ -81,7 +81,8 @@ export function sumPoints(questions: Question[]): number {
  * Consumes an array of questions and returns the sum total of the PUBLISHED questions.
  */
 export function sumPublishedPoints(questions: Question[]): number {
-    return 0;
+    const result = sumPoints(getPublishedQuestions(questions));
+    return result;
 }
 
 /***
